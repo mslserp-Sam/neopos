3205a5fccb3f03b722290400ce2a361a0e402f8e
jabs04 <mslserp.website3.ph@gmail.com>
2024-02-24 17:03:33 +0800

    testt

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -257,7 +257,7 @@ class HomeController extends Controller
             })
             ->filterColumn('display_name',function($query,$keyword){
                 $query->whereHas('provider',function ($q) use($keyword){
-                    $q->where('email','like','%'.$keyword.'%');
+                    $q->where('display_name','like','%'.$keyword.'%');
                 });
             })
             // ->editColumn('display_name', function ($query) {
