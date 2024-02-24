27f74d06c9aee3479bb9aac682e375b44807ee49
jabs04 <mslserp.website3.ph@gmail.com>
2024-02-25 01:55:48 +0800

    testtt

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -323,11 +323,10 @@ class HomeController extends Controller
                 return isset($totalkomi) ? $totalkomi : 0;
             })
             ->editColumn('neo_comm', function($query) {
-                $e = DB::table('users')->where('id', $query->id)
-                ->rightJoin('bookings', 'users.id', '=', 'bookings.provider_id')
-                ->rightJoin('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')
-                ->select('*', 'bookings.id AS booking_new_id', 'bookings.status AS booking_status');
-                return $e->booking_new_id;
+                $ee = User::query();
+                $ee = $ee->where('id', $query->id)
+
+                return $e->first_name;
             })
             ->editColumn('total_completed', function($query) {
                 $totalCompleted = DB::table('bookings')->where('provider_id', $query->id)->where('status', 'completed')->count();
