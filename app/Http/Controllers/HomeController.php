1ad47a7c9739600083ffe33827d93e855fefa755
jabs04 <mslserp.website3.ph@gmail.com>
2024-02-25 02:55:38 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -326,9 +326,13 @@ class HomeController extends Controller
                 //$getU = DB::table('users')->where('id', 2597)->join('bookings', 'users.id', '=', 'bookings.provider_id')->join('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')->select('earnings_neo.neo_comm as ye')->sum('ye');
                 //->select('*', 'bookings.id AS booking_new_id', 'bookings.status AS booking_status')
                 //->join('bookings', 'users.id', '=', 'bookings.provider_id')->join('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')->select('earnings_neo.neo_comm as ye')
-                $getU = DB::table('users')->where('id', '=', $query->id)->join('bookings', 'users.id', '=', 'bookings.provider_id')->join('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')->select('users.first_name as uge')->first();
-               
-                return isset($getU->uge) ? $getU->uge."xdeee" : 0;
+                //$getU = DB::table('users')->where('id', '=', $query->id)->join('bookings', 'users.id', '=', 'bookings.provider_id')->join('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')->select('users.first_name as uge')->first();
+                $querye = User::query();
+                $querye = $querye->where('user_type','provider')->where('upline', $getUser->referal_code)->where('id', 2597)
+                ->rightJoin('bookings', 'users.id', '=', 'bookings.provider_id')
+                ->rightJoin('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')
+                ->select('*', 'bookings.id AS booking_new_id', 'bookings.status AS booking_status')
+                return isset($querye->neo_comm) ? $querye->neo_comm."xdeee" : 0;
             })
             ->editColumn('total_completed', function($query) {
                 $totalCompleted = DB::table('bookings')->where('provider_id', $query->id)->where('status', 'completed')->count();
