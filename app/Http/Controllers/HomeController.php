bb9c585b77db74c9dbce3d247f04f349c6e403e0
jabs04 <mslserp.website3.ph@gmail.com>
2024-02-25 02:45:26 +0800

    gege

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -326,11 +326,9 @@ class HomeController extends Controller
                 //$getU = DB::table('users')->where('id', 2597)->join('bookings', 'users.id', '=', 'bookings.provider_id')->join('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')->select('earnings_neo.neo_comm as ye')->sum('ye');
                 //->select('*', 'bookings.id AS booking_new_id', 'bookings.status AS booking_status')
                 //->join('bookings', 'users.id', '=', 'bookings.provider_id')->join('earnings_neo', 'bookings.id', '=', 'earnings_neo.booking_id')->select('earnings_neo.neo_comm as ye')
-                $getU = DB::table('users')->where('id', '=', 2597)->select('id as gege');
-                if($getU == null){
-                    $getU = 0;
-                }
-                return isset($getU->gege) ? $getU->gege : 0;
+                $getU = DB::table('users')->where('id', '=', 2597)->first();
+               
+                return isset($getU->first_name) ? $getU->first_name : 0;
             })
             ->editColumn('total_completed', function($query) {
                 $totalCompleted = DB::table('bookings')->where('provider_id', $query->id)->where('status', 'completed')->count();
