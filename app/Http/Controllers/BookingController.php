523c9f4f635f6fc107188d9ae09087ad94df7375
jabu04 <mslserp.website3.ph@gmail.com>
2024-02-27 02:13:38 +0800

    test

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -226,12 +226,21 @@ class BookingController extends Controller
     public function sp_search_upline(Request $request)
     {
         $name = $request->email;
-         
+        $ref_id = $request->refid;
+        $lagayan = [];
         $data = DB::table('users')->where('user_type', 'Neopreneur')->where('email', $name)->first();
         if($data == null){
            return response()->json(['data'=> 'none' ,'status' => 'error']);  
         }else{
-            return response()->json(['data'=> $data ,'status' => 'success','ref_ng_upline' => $request->refid]); 
+            $neo = DB::table('users')->where('user_type', 'Neopreneur')->where('referal_id', $ref_id)->first();
+            if($neo->upline){
+                $lagayan['upline_referal'] = $neo->referal_code;
+                $lagayan['upline_upline'] = $neo->upline;
+                return response()->json(['data'=> $data ,'status' => 'success','lagayan' => $lagayan]); 
+            }else{
+                return response()->json(['data'=> 'none' ,'status' => 'error']);
+            }
+            
         }
         
     }
