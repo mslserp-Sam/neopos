f16994dd307614c8141d28af9edc20cc05afa5e7
jabu04 <mslserp.website3.ph@gmail.com>
2024-02-27 19:34:52 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -380,7 +380,7 @@ class HomeController extends Controller
          $tableDatap[] = [];
          $query = DB::table('users')->where('user_type', 'Neopreneur')->get();
          foreach($query as $val){
-            $tableDatap[] = [
+            $tableDatap[] .= [
                     'id', $val->id,
                     'display_name' => $val->display_name
             ];
