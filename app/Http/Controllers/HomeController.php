88a215726c6df85d0801aaa358fdc3b6f1d2e253
jabu04 <mslserp.website3.ph@gmail.com>
2024-02-27 08:17:06 +0800

    test table

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -392,7 +392,8 @@ class HomeController extends Controller
         }   
         return $datatable->eloquent($query)
             ->editColumn('display_name', function($query){
-                return '<a class="btn-link btn-link-hover" >'.$query->display_name.'</a>';
+                //return '<a class="btn-link btn-link-hover" >'.$query->display_name.'</a>';
+                return $query;
             })
             ->filterColumn('display_name',function($query,$keyword){
                 $query->where('display_name','like','%'.$keyword.'%');
