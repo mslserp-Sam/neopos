a0fd671a4a59ff2768f34f09b1c964b7f86fa403
mslserp-Sam <mslserp.website1.ph@gmail.com>
2024-02-27 18:27:42 +0800

    test

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -375,8 +375,7 @@ class HomeController extends Controller
         
         $getUser = auth()->user();
 
-        $neoData = $query->where('user_type', 'Neopreneur')->where('upline', $getUser->referal_code);
-        $query   = $query->where('user_type', 'provider')->where('upline', $neoData->referal_code);
+        $query = $query->where('user_type', 'provider')->where('upline', $getUser->referal_code);
         
         return $datatable->eloquent($query)
             ->editColumn('display_name', function($query){
