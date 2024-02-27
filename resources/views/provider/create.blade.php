fa496afe193197e5af11ab6d2ac62a5bfd01301e
jabu04 <mslserp.website3.ph@gmail.com>
2024-02-27 22:59:14 +0800

    testt

--- resources/views/provider/create.blade.php
+++ resources/views/provider/create.blade.php
@@ -154,12 +154,12 @@
                         @if(isset($providerdata->upline))
                         <div class="row">
                             @php $getUpline = Illuminate\Support\Facades\DB::table('users')->where('referal_code', $providerdata->upline)->first(); @endphp        
-                            <div class="form-group col-md-6 col-sm-12">
+                            <div class="form-group col-lg-6 col-sm-12">
                                 <label class=""form-control-label>Neopreneur</label>
                                 <input type="text" class="form-control " value="{{ $getUpline->first_name }} {{ $getUpline->last_name }}" >
                             </div>
                             @php $getProviderUpline = Illuminate\Support\Facades\DB::table('users')->where('referal_code', $getUpline->upline)->first(); @endphp
-                            <div class="form-group col-md-6 col-sm-12">
+                            <div class="form-group col-lg-6 col-sm-12">
                                 <label class=""form-control-label>Upline</label>
                                 <input type="text" class="form-control " value="{{ $getProviderUpline->first_name }} {{ $getProviderUpline->last_name }}" >
                             </div>
