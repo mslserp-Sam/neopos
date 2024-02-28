8ae0efee5dba79fc48dd00b88ce917de525472e2
jabu04 <mslserp.website3.ph@gmail.com>
2024-02-29 06:48:10 +0800

    test

--- app/Http/Controllers/BookingController.php
+++ app/Http/Controllers/BookingController.php
@@ -190,14 +190,19 @@ class BookingController extends Controller
         if($users){
             $update = DB::table('users')->where('id', $id)->update(['upline' => $users->referal_code, 'neo_neo_id' => $neo_id]);
             if($update){
-                $getProviders = DB::table('users')->where('sp_neo_id', $id)->update(['sp_upline_id' => $neo_id]);
-                if($getProviders){
-                    return response()->json(['data'=> $users ,'status' => 'success']); 
+                $if_neo_has_provider = DB::table('users')->where('user_type', 'provider')->where('sp_neo_id', $id)->first();
+                if($if_neo_has_provider){
+                    $updateSPupline = DB::table('users')->where('sp_neo_id', $id)->update(['sp_upline_id' => $neo_id]);
+                    if($updateSPupline){
+                        return response()->json(['data'=> $users ,'status' => 'success']); 
+                    }else{
+                        return response()->json(['data'=> "a" ,'status' => 'error']); 
+                    }
                 }else{
                     return response()->json(['data'=> "a" ,'status' => 'error']); 
                 }
             }else{
-                
+                return response()->json(['data'=> "a" ,'status' => 'error']); 
             }
         }else{
             return response()->json(['data'=> "a" ,'status' => 'error']); 
